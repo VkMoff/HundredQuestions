e086594113da4773ac72d71896e47fbc494dcff0
VkMoff <148569389+VkMoff@users.noreply.github.com>
2024-03-18 11:20:45 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
+def read():
+    file = pd.ExcelFile('/workspaces/codespaces-flask/_100 вопросов мастеру.xlsx')
+    data_frame_dict = {}
+    for sheet_name in file.sheet_names:
+        data_frame_dict[sheet_name] = pd.read_excel(file, sheet_name, index_col=0)
+    return data_frame_dict
+
+dfs = read()
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    return render_template('index.html', sheets=dfs.keys())
+
+@app.route('/question/', methods=['POST'])
+def question():
+    sheet = request.form.get('index')
+    sample_obj = dfs[sheet].sample()
+    dfs[sheet].drop(sample_obj.index, inplace=True)
+    left_amount = dfs[sheet].shape[0]
+    return render_template('question.html',
+                            name=sample_obj.iloc[0, 0],
+                            work=sample_obj.iloc[0, 1],
+                            job=sample_obj.iloc[0, 2],
+                            question=sample_obj.iloc[0, 3],
+                            link=sheet,
+                            left_amount=left_amount)
